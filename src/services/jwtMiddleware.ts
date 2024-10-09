be49507b6e574e1166daabfb523f4089dda7dab8
thelastgoth128 <thindwacyrus86@gmail.com>
2024-10-09 11:47:52 +0100

    service to get and decode the cookie from the frontend

---
+++ src/services/jwtMiddleware.ts
@@ -0,0 +1,22 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+import * as jwt from 'jsonwebtoken'
+
+
+
+@Injectable()
+export class JwtMiddleware implements NestMiddleware{
+    use(req: Request, res: Response,next: NextFunction){
+        const token = req.cookies['jwt'];
+        if(token){
+            try{
+                const decoded = jwt.verify(token,'hello')
+                req.user = decoded as any
+
+            }catch(error){
+                console.error('JWT verification failed:', error)
+            }
+        }
+        next()
+    }
+}
